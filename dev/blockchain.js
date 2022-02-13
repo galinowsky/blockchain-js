2ee20a5313a2eed196c9cf191ccbea8554ae6a65
Jakub Galinski <jakubgalinski@Jakubs-MacBook-Pro.local>
2022-02-13 11:19:22 +0100

    chain is valid method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -8,7 +8,7 @@ function Blockchain() {
     this.pendingTransactions = [];
     this.currentNodeUrl = currentNodeUrl;
     this.networkNodes = [];
-    this.createNewBlock(100,'0','0');
+    this.createNewBlock(100, '0', '0');
 }
 
 
@@ -43,13 +43,13 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
         amount,
         sender,
         recipient,
-        transactionId :uuid.v1().split('-').join("")
+        transactionId: uuid.v1().split('-').join("")
     };
 
     return newTransaction;
 
 };
-Blockchain.prototype.addTransactionToPendingTransactions = function(newTransaction) {
+Blockchain.prototype.addTransactionToPendingTransactions = function (newTransaction) {
     this.pendingTransactions.push(newTransaction);
     return this.getLastBlock()['index'];
 };
@@ -64,22 +64,47 @@ Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, 
     return hash;
 };
 
-Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData) {
-    let nonce = 0 ;
-    let hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-    while (hash.substring(0,4) !=='0000'){
+    while (hash.substring(0, 4) !== '0000') {
         nonce++;
-        hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
     }
-return nonce;
+    return nonce;
 };
 
-Blockchain.prototype.isGivenNodeAdressCurrent = function(nodeAdress){
+Blockchain.prototype.isGivenNodeAdressCurrent = function (nodeAdress) {
 
-   return  this.currentNodeUrl === nodeAdress;
+    return this.currentNodeUrl === nodeAdress;
 };
-Blockchain.prototype.isNodeRegistered = function(nodeAdress){
+Blockchain.prototype.chainIsValid = function (blockchain) {
+    let validChain = true
+    // this.chain.forEach()
+    for (let i = 1; i < blockchain.length; i++) {
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i - 1];
+        const blockHash = this.hashBlock(prevBlock.hash, { transactions: currentBlock.transactions, index: currentBlock.index },currentBlock.nonce);
+        console.log(blockHash.substring(0, 4) !== '0000');
+        console.log(blockHash.substring(0, 4));
+        
+        console.log("___");
+        if (blockHash.substring(0, 4) !== '0000') validChain = false
+        if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false
+
+    }
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock.nonce === 100;
+    const correctPreviousBlockHash = genesisBlock.previousBlockHash === '0';
+    const correctHash = genesisBlock.hash = '0';
+    const correctTransacrions = genesisBlock.transactions.length === 0;
+
+    if (!(correctNonce && correctPreviousBlockHash && correctHash && correctTransacrions)) validChain = false
+    return validChain
+};
+
+Blockchain.prototype.isNodeRegistered = function (nodeAdress) {
     return this.networkNodes.includes(nodeAdress);
 };
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,25 +1,107 @@
-const  Blockchain  = require('./blockchain');
+const Blockchain = require('./blockchain');
 
 
 
-const hubiCoin  = new Blockchain();
-// hubiCoin.createNewBlock(12131,"234WETW",'3214WEF4R2');
-// hubiCoin.createNewTransaction(123,"marcin","michas");
-// hubiCoin.createNewTransaction(41,"dupa","jaja");
-// hubiCoin.createNewTransaction(2342123,"pupi","hubi");
-// // hubiCoin.createNewBlock(435432,"2WEFWSAF",'23567YRTH');
+const hubiCoin = new Blockchain();
 
-// // hubiCoin.createNewBlock(2341,'23RQWFQW4','ADSF432RQW')
-// // hubiCoin.createNewBlock(12,'23R32QWFQW4','2342342342')
-// // hubiCoin.createNewBlock(234231,'23RQW234FQW4','23434')
-// // console.log(hubiCoin.chain[1].transactions);
-// console.log(hubiCoin.pendingTransactions);
-console.log(hubiCoin);
-// const previousBlockHash = '142451243543DFAWER4Q3';
-// const currentBlockData = [
-//     { amount: 123, sender: 'marcin', recipient: 'michas' },
-//     { amount: 41, sender: 'dupa', recipient: 'jaja' },
-//     { amount: 2342123, sender: 'pupi', recipient: 'hubi' }
-//   ];
+const bc1 = {
+    "chain": [
+        {
+            "index": 1,
+            "timeStamp": 1644358790605,
+            "transactions": [],
+            "nonce": 100,
+            "hash": "0",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 2,
+            "timeStamp": 1644358813688,
+            "transactions": [
+                {
+                    "amount": 123123,
+                    "sender": "hubi",
+                    "recipient": "pupi",
+                    "transactionId": "45e6a5c0892d11ec853211b7e1789ea0"
+                }
+            ],
+            "nonce": 4986,
+            "hash": "0000ba83e68c6ff85116af96cd1bc88ec4a5eeae2195241e5812ef4b2664336b",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 3,
+            "timeStamp": 1644358837425,
+            "transactions": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "3b6d1160892d11ec853211b7e1789ea0",
+                    "transactionId": "493fbbd0892d11ec853211b7e1789ea0"
+                },
+                {
+                    "amount": 12554,
+                    "sender": "michas",
+                    "recipient": "pupi",
+                    "transactionId": "53379d10892d11ec853211b7e1789ea0"
+                },
+                {
+                    "amount": 232411,
+                    "sender": "dupa",
+                    "recipient": "jaja",
+                    "transactionId": "548fb300892d11ec853211b7e1789ea0"
+                }
+            ],
+            "nonce": 4469,
+            "hash": "0000130e7afcea520df8b03ff7eb54f9e24e8f06aebb580cb59c21854a21ec11",
+            "previousBlockHash": "0000ba83e68c6ff85116af96cd1bc88ec4a5eeae2195241e5812ef4b2664336b"
+        },
+        {
+            "index": 4,
+            "timeStamp": 1644358854280,
+            "transactions": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "3b6d1160892d11ec853211b7e1789ea0",
+                    "transactionId": "5765dd70892d11ec853211b7e1789ea0"
+                },
+                {
+                    "amount": 12554,
+                    "sender": "michas",
+                    "recipient": "pupi",
+                    "transactionId": "5d6a1650892d11ec853211b7e1789ea0"
+                },
+                {
+                    "amount": 232411,
+                    "sender": "dupa",
+                    "recipient": "jaja",
+                    "transactionId": "5eae5620892d11ec853211b7e1789ea0"
+                },
+                {
+                    "amount": 123123,
+                    "sender": "hubi",
+                    "recipient": "pupi",
+                    "transactionId": "6010f360892d11ec853211b7e1789ea0"
+                }
+            ],
+            "nonce": 28755,
+            "hash": "0000f79fe2c420b75d64c3f12a96c523d7c9df3bcd435d07912c3a03351810de",
+            "previousBlockHash": "0000130e7afcea520df8b03ff7eb54f9e24e8f06aebb580cb59c21854a21ec11"
+        }
+    ],
+    "pendingTransactions": [
+        {
+            "amount": 12.5,
+            "sender": "00",
+            "recipient": "3b6d1160892d11ec853211b7e1789ea0",
+            "transactionId": "61716dc0892d11ec853211b7e1789ea0"
+        }
+    ],
+    "currentNodeUrl": "http://localhost:3001",
+    "networkNodes": [
+        "http://localhost:3002"
+    ]
+}
 
-//   console.log(hubiCoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(hubiCoin.chainIsValid(bc1.chain));
